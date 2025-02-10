d52acda8025e77b4126c802a71b748ff65b0c496
Santoshtmits <santosh@tmaitservices>
2025-02-10 13:30:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-4xl font-bold text-center text-blue-500">Hello, React!</h1>
+    </div>
+  );
+}
+
+export default App;
